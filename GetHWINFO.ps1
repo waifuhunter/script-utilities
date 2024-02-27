9ce0550bb4e98ad6231fe994fb1b1cc96cbd1efa
Torstein Aakvaag <torstein@aakvaag.net>
2024-02-27 14:24:10 +0100

    Upload hwhash script

---
+++ GetHWINFO.ps1
@@ -0,0 +1,13 @@
+﻿$hostname = $env:COMPUTERNAME
+$grouptag = Read-Host -Prompt "Input GroupTag"
+$file = ".\$hostname.$grouptag.csv"
+.\Get-WindowsAutoPilotInfo.ps1 -outputfile $file -GroupTag $grouptag
+$filexist = Test-Path -Path "D:\$hostname.$grouptag.csv" -Include *.csv
+if ($filexist -eq $true){
+	Write-host 'The HWID CSV have been created.'
+	Exit
+}	
+else {
+	Write-host 'The HWID CSV have not been created.'
+	Exit
+}
